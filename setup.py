c462cd5044afc550078cf76913fdd79f618aae70
Cyrus Sandhu <cyrussandhu@gmail.com>
2024-08-27 15:32:38 -0700

    exception change

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from typing import List
 
 HYPEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements 
-    '''
     
     requirements=[]
     with open(file_path) as file_obj:
