689255ac077d05904afcf12297545b3eb4a68a47
Cyrus Sandhu <cyrussandhu@gmail.com>
2024-09-02 01:03:14 -0700

    model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -15,4 +15,29 @@ from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
 from src.exception import CustomException
-from src.logger import loggin
+from src.logger import logging
+
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join('artifacts', 'model.pkl')
+    
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+        
+    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+        try:
+            logging.info("Splitting training and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            
+            
+            
+        except:
+            pass
